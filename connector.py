ef905882c4f18c8ae4779853b421ba3942626daf
devim <developerimalka@gmail.com>
2022-06-12 20:12:41 +0530

    removed extra added imports and fixed xlswriter index not droping

--- connector.py
+++ connector.py
@@ -1,56 +1,13 @@
-from ast import Not
-from calendar import day_abbr
-from cmath import inf
-from multiprocessing import connection
-from operator import index
-from sqlite3 import Cursor
-from unittest.result import failfast
 import mysql.connector
-from mysql.connector import errorcode
-from numpy import save
-
-import pandas as pd
-from requests import NullHandler
-from sqlalchemy import false
-#importing Queries
 from Queries import *
-
-#Location Ip's
-from locations import locs
-
+from locations import *
+from lib import *
+import openpyxl
 from env import *
-
-startDate='2022-05-01'
-endDate='2022-05-15'
-
-from lib import fileChecker,logwriter
-
-    
-
-from cmath import inf
-from multiprocessing import connection
-from operator import index
-from sqlite3 import Cursor
-from unittest.result import failfast
-import mysql.connector
-from mysql.connector import errorcode
-
+import xlwt
 import pandas as pd
-from sqlalchemy import false
-#importing Queries
-from Queries import *
 
-#Location Ip's
-from locations import locs
-
-from env import *
-
-
-from Test_Variables import location_test as loctest
-
-from lib import fileChecker,logwriter
-
-    
+from mysql.connector import errorcode 
 
 def executor(QUERY):
     dataFrameStack = []
@@ -79,34 +36,27 @@ def executor(QUERY):
                     
                     cursor = cnx.cursor()
                     cursor.execute(QUERY)
-                    df = pd.DataFrame(cursor)
-                   
+                    df = pd.DataFrame(cursor.fetchall())
+                    df = df.reset_index(drop=True)              
                     print(df)
 
 
-                    # if not df.empty:
-                    #     if dataFrameStack is not None:
-                    #         dataFrameStack = dataFrameStack.append(df)
-
-                        
-                    # else:
-                    #     dataFrameStack = df
-
-                    dataFrameStack.append(df)
-                    print("len of dataframestack is {}".format(len(dataFrameStack)))
-                      
                  
-                    print('\n\n\n\n\n***********************')
-                    print(dataFrameStack)
+                    if not df.empty:
+                        dataFrameStack.append(df)
+             
+                 
+                                    
                     field_names = [ i[0] for i in  cursor.description]
                     print(field_names)
                         
                     xlswriter = pd.ExcelWriter('{}/{}.xls'.format(type,loc),engine='openpyxl')
 
                     if not df.empty:
-                        df.columns = field_names  
-                      
-                        df.to_excel(xlswriter,index=false)
+                       
+                        df.columns = field_names
+                        df = df.reset_index(drop=True)
+                        df.to_excel(xlswriter,index=False)
 
                         xlswriter.save()
                     else:
