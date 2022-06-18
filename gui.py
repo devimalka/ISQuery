e634f50c62b0b6924248ae9471a1e9bcab8ba8a2
devim <developerimalka@gmail.com>
2022-06-18 22:12:46 +0530

    added main window two buttons

--- gui.py
+++ gui.py
@@ -2,19 +2,9 @@
 from PyQt5.QtCore import QSize,Qt
 from PyQt5.QtWidgets import QApplication,QWidget,QMainWindow,QPushButton,QLabel,QCheckBox,QBoxLayout,QVBoxLayout,QHBoxLayout
 from PyQt5.QtGui import QPalette,QColor
-
+from PyQt5.QtCore import pyqtSlot
 
 import sys
-
-class Color(QWidget):
-
-    def __init__(self, color):
-        super(Color, self).__init__()
-        self.setAutoFillBackground(True)
-
-        palette = self.palette()
-        palette.setColor(QPalette.Window, QColor(color))
-        self.setPalette(palette)
 
 
 class MainWindow(QMainWindow):
@@ -25,23 +15,33 @@ class MainWindow(QMainWindow):
         
         self.setFixedSize(360,360)
 
+        #create qwidget because can't add qlayout to mainwindow
+        self.wid = QWidget()
 
-        layout = QVBoxLayout()
-        layout.addWidget(Color('red'))
-        layout.addWidget(Color('green'))
+        #set qwidget as centralwidget
+        self.setCentralWidget(self.wid)
+        
+        #create layout
+        self.layout = QVBoxLayout()
 
-        layout2 = QHBoxLayout()
-        layout2.addWidget(Color('blue'))
-        layout2.addWidget(Color('black'))
+        self.button = QPushButton()
+        self.button.setText("Doc Tally")
+        self.layout.addWidget(self.button)
+        self.button.clicked.connect(self.doctally)
 
-        layout.addLayout(layout2)
 
+        self.button2 = QPushButton('btn2')
+        self.layout.addWidget(self.button2,2)
 
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
+        #add layout to central widget
+        self.wid.setLayout(self.layout)
 
-  
+    def doctally(self):
+            self.w = QWidget()
+            self.w.label = QLabel("test")
+            self.w.show()
+
+     
 
 app = QApplication([])
 
