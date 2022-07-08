e4568c9589e55c1cafc1f04730a883f0d5434cac
devim <developerimalka@gmail.com>
2022-07-08 16:09:41 +0530

    Improved servers status.py

--- ServerStatus.py
+++ ServerStatus.py
@@ -1,6 +1,6 @@
 import socket
 import mysql.connector 
-from locations import locs
+from locations import LocationDictionary
 
 from var import bcolors
 import time
@@ -42,24 +42,51 @@ os.system('color')
 
 # ping_server('192.9.236.1',3306)
 
+class serverUPorDown():
+    def __init__(self,iplist):
+        self.iplist = iplist
+        self.failedlist = []
+        self.active = []
+
+    def serverDownOrUp(self):
+
+        while True:
+            for tp,ip_info in self.iplist.items():
+
+                for ip,name in ip_info.items():
+
+                    try:
+                        connection = mysql.connector.connect(user='supun.c',password='spn.c',host=ip,database='marksys') 
+                        if connection.is_connected():
+                            self.appendtolist(ip,'active')
+                            self.removefromlist(ip,'failed')
+                            print("Connection success to {:^15}{:<20} All Active count [{:<2}] Failed Count [{:>2}]".format(name,tp,len(self.active),len(self.failedlist)))
+                            connection.close()
+                            time.sleep(1)
+
+                    except:
+                        self.appendtolist(ip,'failed')
+                        self.removefromlist(ip,'active')
+                        # print("f{bcolors.FAIL}connection failed to %s %s{bcolors.ENDC}"%(name,tp))
+                        print(bcolors.FAIL+"Connection Failed  to {:^15}{:<20} All Active count [{:<2}] Failed Count [{:>2}]".format(name,tp,len(self.active),len(self.failedlist))+bcolors.ENDC)
 
 
-def serverDownOrUp(iplist):
-
-    while True:
-        for tp,ip_info in iplist.items():
-
-            for ip,name in ip_info.items():
-
-                try:
-                    connection = mysql.connector.connect(user='supun.c',password='spn.c',host=ip,database='marksys') 
-                    if connection.is_connected():
-                        print("Connection successfull to {} {}".format(name,tp))
-                        connection.close()
-                        time.sleep(1)
-
-                except:
-                    print(f"{bcolors.FAIL}connection failed to %s %s{bcolors.ENDC}"%(name,tp))
-
-
-serverDownOrUp(locs)                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+    def appendtolist(self,ip,listype):
+        if listype == 'failed':
+            if ip not in self.failedlist:
+                self.failedlist.append(ip)
+        else:
+            if ip not in self.active:
+                self.active.append(ip)
+            
+    def removefromlist(self,ip,listype):
+        if listype == 'failed':
+            if ip in self.failedlist:
+                self.failedlist.remove(ip) 
+        else:
+            if ip in self.active:
+                self.active.remove(ip)     
+                
+                
+runipcheck = serverUPorDown(LocationDictionary)
+runipcheck.serverDownOrUp()                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
